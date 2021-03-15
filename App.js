f1698465e7f266ef22716d49eb9da624c1c929ed
Michell Zipitría <mzipi@outlook.com>
2021-03-14 23:29:15 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+  <React.StrictMode>
+    <div class="wrapper">
+      <div class="item1">BIENVENIDO A REACT</div>
+      <div class="item2">EL SITIO ESTÁ EN CONSTRUCCIÓN</div>
+    </div>
+  </React.StrictMode>,
+document.getElementById('root')
+);
